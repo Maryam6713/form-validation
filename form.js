50868a566fe2f8b68bfa19c57da9980f46c99962
Maryam6713 <maryammalik6713@gmail.com>
2024-09-14 16:10:19 +0500

    initial commit

--- form.js
+++ form.js
@@ -1,52 +1,55 @@
 function formSub() {
-    var emailValid = /^[\w\-\.\+]+\@[a-zA-Z0-9\. \-]+\.[a-zA-z0-9]{2,4}$/;
-    var passwordValid =/ (?=^.{6,}$)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[^A-Za-z0-9]).*/;
-    var cnicValid = /^[0-9]{13}$/;
-    var contactValid = /^[0-9]{11}$/;
+    // Validation regex patterns
+    var emailValid = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,4}$/;
+    var passwordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/; 
+    var cnicValid = /^[0-9]{5}-[0-9]{7}-[0-9]{1}$/;
+    var contactValid = /^03[0-9]{2}[0-9]{7}$/;
 
-    var targetField = document.getElementById("exampleInputEmail1");
+    // Target fields
+    var emailField = document.getElementById("exampleInputEmail1");
     var passwordField = document.getElementById("exampleInputPassword1");
     var cnicField = document.getElementById("cnicNumber");
     var contactField = document.getElementById("contactNumber");
 
     // Email validation
-    if (targetField.value.length === 0 || targetField.value !== emailValid) {
-        alert("Please enter a valid email address");
-        targetField.focus();
-        targetField.style.background = "yellow";
+    if (!emailValid.test(emailField.value)) {
+        swal("Please enter a valid email address");
+        emailField.focus();
+        emailField.style.background = "yellow";
         return false;
     }
 
     // Password validation
-    if (passwordField.value.length === 0 || passwordField.value !== passwordValid) {
-        alert("Please enter a strong password with at least 8 characters, including letters and numbers");
+    if (!passwordValid.test(passwordField.value)) {
+        swal("Password must contain at least 6 characters, including uppercase, lowercase, number, and a special character.");
         passwordField.focus();
         passwordField.style.background = "yellow";
         return false;
     }
 
     // CNIC validation
-    if (cnicField.value.length === 0 || !cnicValid.test(cnicField.value)) {
-        alert("Please enter a valid 13-digit CNIC number");
+    if (!cnicValid.test(cnicField.value)) {
+        swal("Please enter a valid 13-digit CNIC number in the format XXXXX-XXXXXXX-X");
         cnicField.focus();
         cnicField.style.background = "yellow";
         return false;
     }
 
     // Contact number validation
-    if (contactField.value.length === 0 || !contactValid.test(contactField.value)) {
-        alert("Please enter an 11-digit contact number");
+    if (!contactValid.test(contactField.value)) {
+        swal("Please enter a valid 11-digit contact number starting with 03");
         contactField.focus();
         contactField.style.background = "yellow";
         return false;
     }
 
-    // Resetting the background color of fields upon successful validation
-    targetField.style.background = "white";
+    // Reset field background colors upon successful validation
+    emailField.style.background = "white";
     passwordField.style.background = "white";
     cnicField.style.background = "white";
     contactField.style.background = "white";
 
-    // If all validations pass, allow the form to be submitted
+    // If all fields are valid, form submission proceeds
+    swal("Form successfully submitted!");
     return true;
 }
